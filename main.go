bcc940384ca7cf5aed01fded50047b780b190a9d
Jan Hettich <janhettich@gmail.com>
2018-07-15 22:16:12 -0700

    add main.go

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+       "fmt"
+       "github.com/google/codesearch/regexp"
+)
+
+func main() {
+     r := regexp.Regexp{}
+     fmt.Println(r)
+     fmt.Println("This is the main program.")
+}
